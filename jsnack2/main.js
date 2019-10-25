fd60444d03508e4d3a5c07ded6d62baaac3d9e32
cabal82 <fabriziogallici@gmail.com>
2019-10-25 14:50:22 +0200

    mod es 2

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -3,11 +3,9 @@ var word1 = prompt('Inserisci la prima parola');
 var word2 = prompt('Inserisci la seconda parola');
 
 if (word1.length < word2.length) {
-  console.log('word1', word1);
-  console.log('word2', word2);
+  console.log(word1 + ' ' + word2);
 } else if (word2.length < word1.length) {
-  console.log('word2', word2);
-  console.log('word1', word1);
+  console.log(word2 + ' ' + word1);
 } else {
   console.log('le parole hanno uguale lunghezza');
 }
