04c9ab24cbddb13924f45f8e6993ec94412c8cc5
cabal82 <fabriziogallici@gmail.com>
2019-10-25 14:35:22 +0200

    es 5 base

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -1,2 +1,12 @@
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire
 // un numero, se è dispari inseriscilo nell’array.
+
+var nums = [];
+var userNum;
+for (var i = 1; i <= 6; i++) {
+  userNum = parseInt(prompt('Inserisci un numero'));
+  if (userNum % 2 !== 0) {
+    nums.push(userNum);
+  }
+}
+console.log(nums);
